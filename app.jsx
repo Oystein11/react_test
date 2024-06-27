9e4fa70b4cb218fc8644bdeeb984a96bae77ff18
Øystein <oystein.koder@gmail.com>
2024-06-27 11:27:09 +0200

    init

---
+++ app.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Landingpage } from "./src/pages/landing_page";
+
+function App () {
+    return (
+        <h1>Hello React!</h1>
+    )
+};
+
+export {App};
---
+++ src/pages/landing_page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Landingpage () {
+    return (
+        <>
+            <h1>Hello!</h1>
+        </>
+    )
+};
+
+export {Landingpage};
